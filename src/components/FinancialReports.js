61fcc7c723c29b603be064504c2154ec63b8daa6
john <jyou@mail.com>
2025-05-21 12:00:01 +0530

    showed individual accounts

--- src/components/FinancialReports.js
+++ src/components/FinancialReports.js
@@ -3,41 +3,44 @@
 import React from 'react';
 
 const generateReports = (accounts) => {
-  const profitLoss = accounts
-    .filter(acc => acc.accountType === 'income' || acc.accountType === 'expense')
-    .reduce((acc, { accountType, previousYearAmount, currentYearAmount }) => {
-      if (accountType === 'income') {
-        acc.previousYearIncome += parseFloat(previousYearAmount || 0);
-        acc.currentYearIncome += parseFloat(currentYearAmount || 0);
-      } else if (accountType === 'expense') {
-        acc.previousYearExpense += parseFloat(previousYearAmount || 0);
-        acc.currentYearExpense += parseFloat(currentYearAmount || 0);
-      }
-      return acc;
-    }, {
-      previousYearIncome: 0,
-      currentYearIncome: 0,
-      previousYearExpense: 0,
-      currentYearExpense: 0,
-    });
+  const profitLoss = {
+    income: [],
+    expense: [],
+    previousYearIncome: 0,
+    currentYearIncome: 0,
+    previousYearExpense: 0,
+    currentYearExpense: 0,
+  };
+  
+  const balanceSheet = {
+    asset: [],
+    liability: [],
+    previousYearAssets: 0,
+    currentYearAssets: 0,
+    previousYearLiabilities: 0,
+    currentYearLiabilities: 0,
+  };
 
-  const balanceSheet = accounts
-    .filter(acc => acc.accountType === 'asset' || acc.accountType === 'liability')
-    .reduce((acc, { accountType, previousYearAmount, currentYearAmount }) => {
-      if (accountType === 'asset') {
-        acc.previousYearAssets += parseFloat(previousYearAmount || 0);
-        acc.currentYearAssets += parseFloat(currentYearAmount || 0);
-      } else if (accountType === 'liability') {
-        acc.previousYearLiabilities += parseFloat(previousYearAmount || 0);
-        acc.currentYearLiabilities += parseFloat(currentYearAmount || 0);
-      }
-      return acc;
-    }, {
-      previousYearAssets: 0,
-      currentYearAssets: 0,
-      previousYearLiabilities: 0,
-      currentYearLiabilities: 0,
-    });
+  // Categorize the accounts and calculate totals
+  accounts.forEach(({ accountType, accountName, previousYearAmount, currentYearAmount }) => {
+    if (accountType === 'income') {
+      profitLoss.income.push({ accountName, previousYearAmount, currentYearAmount });
+      profitLoss.previousYearIncome += parseFloat(previousYearAmount || 0);
+      profitLoss.currentYearIncome += parseFloat(currentYearAmount || 0);
+    } else if (accountType === 'expense') {
+      profitLoss.expense.push({ accountName, previousYearAmount, currentYearAmount });
+      profitLoss.previousYearExpense += parseFloat(previousYearAmount || 0);
+      profitLoss.currentYearExpense += parseFloat(currentYearAmount || 0);
+    } else if (accountType === 'asset') {
+      balanceSheet.asset.push({ accountName, previousYearAmount, currentYearAmount });
+      balanceSheet.previousYearAssets += parseFloat(previousYearAmount || 0);
+      balanceSheet.currentYearAssets += parseFloat(currentYearAmount || 0);
+    } else if (accountType === 'liability') {
+      balanceSheet.liability.push({ accountName, previousYearAmount, currentYearAmount });
+      balanceSheet.previousYearLiabilities += parseFloat(previousYearAmount || 0);
+      balanceSheet.currentYearLiabilities += parseFloat(currentYearAmount || 0);
+    }
+  });
 
   return { profitLoss, balanceSheet };
 };
@@ -51,24 +54,38 @@ const FinancialReports = ({ accounts }) => {
       <table>
         <thead>
           <tr>
-            <th></th>
+            <th>Account</th>
             <th>Previous Year</th>
             <th>Current Year</th>
           </tr>
         </thead>
         <tbody>
+          {profitLoss.income.map((account, index) => (
+            <tr key={`income-${index}`}>
+              <td>{account.accountName}</td>
+              <td>{account.previousYearAmount}</td>
+              <td>{account.currentYearAmount}</td>
+            </tr>
+          ))}
+          {profitLoss.expense.map((account, index) => (
+            <tr key={`expense-${index}`}>
+              <td>{account.accountName}</td>
+              <td>{account.previousYearAmount}</td>
+              <td>{account.currentYearAmount}</td>
+            </tr>
+          ))}
           <tr>
-            <td>Income</td>
+            <td><strong>Total Income</strong></td>
             <td>{profitLoss.previousYearIncome}</td>
             <td>{profitLoss.currentYearIncome}</td>
           </tr>
           <tr>
-            <td>Expenses</td>
+            <td><strong>Total Expenses</strong></td>
             <td>{profitLoss.previousYearExpense}</td>
             <td>{profitLoss.currentYearExpense}</td>
           </tr>
           <tr>
-            <td>Net Profit</td>
+            <td><strong>Net Profit</strong></td>
             <td>{profitLoss.previousYearIncome - profitLoss.previousYearExpense}</td>
             <td>{profitLoss.currentYearIncome - profitLoss.currentYearExpense}</td>
           </tr>
@@ -79,19 +96,33 @@ const FinancialReports = ({ accounts }) => {
       <table>
         <thead>
           <tr>
-            <th></th>
+            <th>Account</th>
             <th>Previous Year</th>
             <th>Current Year</th>
           </tr>
         </thead>
         <tbody>
+          {balanceSheet.asset.map((account, index) => (
+            <tr key={`asset-${index}`}>
+              <td>{account.accountName}</td>
+              <td>{account.previousYearAmount}</td>
+              <td>{account.currentYearAmount}</td>
+            </tr>
+          ))}
+          {balanceSheet.liability.map((account, index) => (
+            <tr key={`liability-${index}`}>
+              <td>{account.accountName}</td>
+              <td>{account.previousYearAmount}</td>
+              <td>{account.currentYearAmount}</td>
+            </tr>
+          ))}
           <tr>
-            <td>Assets</td>
+            <td><strong>Total Assets</strong></td>
             <td>{balanceSheet.previousYearAssets}</td>
             <td>{balanceSheet.currentYearAssets}</td>
           </tr>
           <tr>
-            <td>Liabilities</td>
+            <td><strong>Total Liabilities</strong></td>
             <td>{balanceSheet.previousYearLiabilities}</td>
             <td>{balanceSheet.currentYearLiabilities}</td>
           </tr>
